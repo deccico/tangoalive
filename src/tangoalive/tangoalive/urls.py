b4cd83d16f51f7313acdd512ed2392932b5bfdb5
Adrián Deccico <deccico@gmail.com>
2016-08-03 22:38:30 +1000

    adding contact page

--- src/tangoalive/tangoalive/urls.py
+++ src/tangoalive/tangoalive/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     url(r'', include('eventos.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^eventos/', include('eventos.urls')),
+    url(r'^contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
