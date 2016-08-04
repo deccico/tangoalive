619f6b536239264ece8269702fda952c7202198f
Adrián Deccico <deccico@gmail.com>
2016-08-04 23:32:36 +1000

    cambio imagen de fondo

--- src/tangoalive/eventos/urls.py
+++ src/tangoalive/eventos/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = "eventos"
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    # ex: /eventos/5/
+    url(r'^(?P<eventos_id>[0-9]+)/$', views.detail, name='detail'),
 ]
