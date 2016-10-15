8c5d7de12da1237574403459c0ab69ecaf5fb560
Adrián Deccico <deccico@gmail.com>
2016-10-15 19:37:36 +1100

    parse precio as float. Somehow sqlite was giving me: Decimal('2.05') for a decimal value...

--- src/tangoalive/eventos/views.py
+++ src/tangoalive/eventos/views.py
@@ -146,7 +146,7 @@ def get_compra_obj(title, quantity, price, external_reference, picture_url):
                 "picture_url": picture_url,
             }
         ],
-        u'payment_methods': {
+        'payment_methods': {
             'installments': 1,
             'excluded_payment_types': [
                 {'id': 'ticket'},
@@ -176,7 +176,7 @@ def buy(request, eventos_id):
 
     #create mp object with the right quantity and event id
     #forward to mp to let the user buy
-    _, url = get_compra_obj(evento, quantity, evento.precio_entrada, evento.id,
+    _, url = get_compra_obj(evento, quantity, float(evento.precio_entrada), evento.id,
                           "http://tangoalive.com/media/{0}".format(evento.image_1))
 
     #redirect to MP site
