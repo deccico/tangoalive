2d8b761d284019bc396e3fbfca68d0712727c706
Adrián Deccico <deccico@gmail.com>
2016-10-29 09:18:24 +1100

    setting static files in a separate dir to fix admin section

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,3 +1,4 @@
 cd /home/adrian/tangoalive/src/tangoalive
 git pull origin master  >> /home/adrian/updatelogs.txt
 python manage.py migrate >> /home/adrian/model_updatelogs.txt
+python manage.py collectstatic --noinput >> /home/adrian/static_files_update.txt
