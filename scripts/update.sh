abda9d12a770b47c2ddce2035a8870049fb0fd69
Adrián Deccico <deccico@gmail.com>
2016-11-07 22:05:46 +1100

    minor cosmetic changes

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,3 +2,4 @@ cd /home/adrian/tangoalive/src/tangoalive
 git pull origin master  >> /home/adrian/updatelogs.txt
 python manage.py migrate >> /home/adrian/model_updatelogs.txt
 python manage.py collectstatic --noinput >> /home/adrian/static_files_update.txt
+touch /home/adrian/tangoalive/src/tangoalive/tangoalive_uwsgi.ini
