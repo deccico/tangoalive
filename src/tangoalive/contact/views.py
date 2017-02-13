56a54586357a63b052879541a9ab059390544ab5
Adrián Deccico <deccico@gmail.com>
2017-02-14 08:51:00 +1100

    adding random acercade

--- src/tangoalive/contact/views.py
+++ src/tangoalive/contact/views.py
@@ -1,6 +1,11 @@
+import random
+
 from django.http import HttpResponse
 from django.template import loader
 
 def about(request):
     template = loader.get_template('contact/about.html')
-    return HttpResponse(template.render({}, request))
+    context = {
+        'img_rnd': '{0:04d}'.format(random.randint(1, 16)),
+    }
+    return HttpResponse(template.render(context, request))
